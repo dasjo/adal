dc8146cf3aff8268b9391f8fb3c828c35e81a9ea
dasjo <dasjogibtsnet@gmail.com>
2011-03-08 11:15:00 +0100

    commit misc

--- sites/default/themes/adal_bright/templates/block-block-1.tpl.php
+++ sites/default/themes/adal_bright/templates/block-block-1.tpl.php
@@ -4,7 +4,9 @@
 
 	<ul class="icons">
 		<li><a href="<?php print base_path() ?>info" rel="lightframe[a|width:980px;height:500px][]" class="info icon"><span>Info</span></a></li>
+<?php /*
 		<li><a href="<?php print base_path() ?>news" rel="lightframe[b|width:980px;height:500px][]" class="news icon"><span>News</span></a></li>
+*/ ?>
 		<li><a href="<?php print base_path() ?>magazine" rel="lightframe[c|width:980px;height:500px][]" class="magazine icon"><span>Magazine</span></a></li>
 		<li><a href="<?php print base_path() ?>contact" rel="lightframe[d|width:980px;height:500px][]" class="contact icon"><span>Contact</span></a></li>
 		<li><a href="http://www.facebook.com/group.php?gid=8197067511" class="facebook icon"><span>Facebook</span></a></li>
